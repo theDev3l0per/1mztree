22a98fe08f4e83bec43a31539e10315bcc4adf95
Glitch (awesome-jet-shear) <none>
2020-11-28 18:03:46 +0000

    🏹🙌 Checkpoint ./js/tree.js:25044483/454

--- js/tree.js
+++ js/tree.js
@@ -21,17 +21,18 @@ addLayer("tree-tab", {
     cols: 2,
     11: {
       title: "Doubler",
-      cost() {return new Decimal(1500)},
+      cost() {return new Decimal(6).pow(getBuyableAmount(this.layer, 11)).times(10)},
       canAfford() {
         return new Decimal(player.points).gte(this.cost())
       },
       unlocked() {return true},
-      display() {return `Double your zero gain`},
+      display() {return `Double your zero gain\n Cost: ${this.cost().round()}\n Effect: ${this.effect().round()}`},
       buy() {
-        
+        setBuyableAmount(this.layer, 11, new Decimal(getBuyableAmount(this.layer, 11)).add(1))
+        player.points = player.points.sub(this.cost())
       },
       effect() {
-        return new Decimal(0)
+        return new Decimal(2).pow(getBuyableAmount(this.layer, 11))
       }
     },
   },
