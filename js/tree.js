4022d731b8885ae84daa3a904af14522ead688b8
Glitch (awesome-jet-shear) <none>
2020-11-28 18:27:41 +0000

    📟🎽 Checkpoint ./js/tree.js:25044483/675

--- js/tree.js
+++ js/tree.js
@@ -35,6 +35,22 @@ addLayer("tree-tab", {
         return new Decimal(2).pow(getBuyableAmount(this.layer, 11))
       }
     },
+    12: {
+      title: "Tripler",
+      cost() {return new Decimal(7).pow(getBuyableAmount(this.layer, 12)).times(1000)},
+      canAfford() {
+        return new Decimal(player.points).gte(this.cost())
+      },
+      unlocked() {return true},
+      display() {return `Triple your zero gain\n Cost: ${this.cost().round()}\n Effect: ${this.effect().round()}`},
+      buy() {
+        setBuyableAmount(this.layer, 12, new Decimal(getBuyableAmount(this.layer, 12)).add(1))
+        player.points = player.points.sub(this.cost())
+      },
+      effect() {
+        return new Decimal(3).pow(getBuyableAmount(this.layer, 12))
+      }
+    },
   },
     tabFormat: ["buyables",["tree", function() {return (layoutInfo.treeLayout ? layoutInfo.treeLayout : TREE_LAYERS)}]]
 })
